739429253abdbda30167b3cfaad2c1052e7eb99d
Jovan Ivezic <jovanivezic@gmail.com>
2019-10-22 13:56:58 +0200

    change url withut reloading

--- app/app.js
+++ app/app.js
@@ -2,3 +2,4 @@ require('./modules/gallery');
 require('./modules/about');
 require('./modules/form');
 require('./modules/learning');
+require('./modules/change-url');
--- app/modules/change-url.js
+++ app/modules/change-url.js
@@ -1,1 +1,20 @@
-history.pushState({ id: 'home'}, 'Home | My App', 'http://localhost:8080/home');
+(function() {
+
+    var links = document.querySelectorAll('.js-nav-link');
+
+    var historyState = {};
+
+    for (var i = 0; i < links.length; i++) {
+
+        links[i].addEventListener('click', function(evt) {
+            evt.preventDefault();
+
+            //Remove hash from URL and replace with desired URL
+            if (history && history.pushState) {
+                //Only do this if history.pushState is supported by the browser
+                history.pushState(historyState, evt.target.innerHTML, evt.target.href);
+            }
+        });
+    }
+
+})();
